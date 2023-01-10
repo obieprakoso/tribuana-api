a3d0595d518b6ca6f23b56835d148a225c877f9f
obieprakoso <obieprakosogmail.com>
2023-01-10 17:33:49 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ const port = process.env.PORT || 3000;
 const swaggerDoc = require("swagger-ui-express");
 const swaggerDocumation = require("./helper/documentation");
 const baseResponse = require("./helper/responseDefault");
+const cors = require("cors");
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", routes);
 app.use("./helper/swagger-ui.css", express.static("./helper/swagger-ui.css"));
 app.use((err, req, res, next) => {
